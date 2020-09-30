a44adef8d4d572558807a570ddb8efc44cb44a25
Erick Castille <ecastille924@gmail.com>
2020-09-30 22:46:19 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,7 @@ class Anagram
   
   def compare(array)
     array.find_all do |word_x|
-      if word_x.split("") == self.word.split
+      if word_x.split("").sort == self.word.split("").sort
     end
   end
   
