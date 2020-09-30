483ae516367af8d5625acaddce11652826c2792b
Erick Castille <ecastille924@gmail.com>
2020-09-30 22:39:38 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word 
+  
+  def initialize (word)
+    @word = word
+  end
+  
+  def compare(array)
+    array.select do |word_x|
+      (@word.split("").sort) == (word_x.split("").sort)
+    end
+  end
+  
+end
